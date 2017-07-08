a8ee13961ef1f9308cfddc3ef97a3377031b6a60
Dan <cohen.dan@husky.neu.edu>
2017-07-08 11:40:20 -0400

    added redirect

--- test/string.emoji.js
+++
@@ -1,6 +0,0 @@
-console.log("You can output reserved emojis as strings, and emojiscript won\'t replace them: ")
-console.log("🔨🏨👶\n")
-
-console.log("You can even use Javascript\'s reserved words as variable names: ")
-let lgpetrjv = "if"
-console.log(lgpetrjv)
